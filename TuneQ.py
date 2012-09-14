f4c91978e456b97cc81c89651482fca2545d4e2d
clindsay107 <clindsay107@gmail.com>
2012-09-14 01:22:52 -0700

    Basic functionality. Responds to browsers with video and the sms with an echo

--- TuneQ.py
+++ TuneQ.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, session
+from flask import Flask, request, redirect, session, get_template_attribute, render_template
 from twilio.rest import TwilioRestClient
 import twilio.twiml
 import os
@@ -10,17 +10,22 @@ TOKEN = '0a1f88c479541c3ff4b1769c5651b964'
 app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
-def replyToSMS():
-
+def showVideo():
 	from_number = request.values.get('From', None)
 	from_contents = request.values.get('Body', None)
 
-	if from_contents is not None and from_number is not None:
-		resp = twilio.twiml.Response()
-		resp.sms('You texted from ' + from_number + 'with the text ' + from_contents)
-		return str(resp)
+	if from_number is not None and from_contents is not None:
+		return replyToSMS(from_number, from_contents)
 	else:
-		return 'You are viewing from a web browser, no text for you!'
+		video = get_template_attribute('index.html', 'vid')
+		url = 'http://www.youtube.com/embed/7W194GQ6fHI'
+		return video(url)
+
+def replyToSMS(from_number, from_contents):
+
+	resp = twilio.twiml.Response()
+	resp.sms('You texted from ' + from_number + 'with the text ' + from_contents)
+	return str(resp)
 
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
